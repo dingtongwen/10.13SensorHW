1ec7870486212421f1fc06ec22bf096f72986492
dingtongwen <ewotid@hotmail.com>
2015-10-20 16:09:30 -0400

    sensors

---
+++ ultrasonic/ultrasonic.ino
@@ -0,0 +1,35 @@
+#define TRIGPIN 10
+#define ECHOPIN 13
+#define LED1 8
+#define LED2 9
+
+long ping() {
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+  return pulseIn(ECHOPIN, HIGH)/58;
+}
+
+void setup() {
+  pinMode(TRIGPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+}
+
+void loop() {
+  long cm = ping();
+  
+  if (cm <= 100) {
+    digitalWrite(LED1, HIGH);
+    delay(cm*2.5 + 50);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    delay(cm*2.5 + 50);
+    digitalWrite(LED2, LOW);
+  }
+  
+  delay(100);
+}
